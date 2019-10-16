ce518dabafafb72b534000e9fec99cdd5a5b2985
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-10-16 16:55:16 +0800

    Initial Commit

---
+++ myvue/src/router.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',//required
+      name: 'home',//optional
+      component: Home//required--unsa nga file ang i-access
+    },
+    {
+      path: '/about',//required
+      name: 'about',//optional
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+})
